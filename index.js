fe984756be4568a1504b9234330c26bd08627483
LD <LDinho@users.noreply.github.com>
2019-05-08 23:05:07 -0400

    add get users endpoint

--- index.js
+++ index.js
@@ -1,14 +1,35 @@
 // implement your API here
-
 const express = require('express');
+
+const db = require('./data/db')
 
 const server = express();
 
+const { find } = db;
+
 server.get('/', (req, res) => {
-  res.send('Hello World');
+  console.log('inside get');
+  // res.send('<h4>Hello World</h4>');
+  res.json("The server is running!");
 
 });
 
+server.get('/api/users', (req, res) => {
+  find()
+    .then((users) => {
+      if (users) {
+        res.status(200).json(users);
+      } else {
+        res.status(500)
+          .json({
+            error: "The users information could not be retrieved."
+          });
+      }
+    })
+    .catch((err) => {
+      res.status(500).send(err);
+    })
+});
 
 // listening
 server.listen(9090, ()=> {
